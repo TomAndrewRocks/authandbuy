c0a21aa377f0bec96ca10ab8bc30fc63bcb9bef1
MarioFerreira <mario@entregali.com.br>
2023-11-25 12:40:47 -0300

    auth and buy #01

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { theme } from "./shared/themes/theme";
+
+export default function Page() {
+  return (
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <Text style={styles.title}>Auth and Buy</Text>
+        </View>
+      </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "expo-router/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "AUTHBUY_ENV",
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+      '@babel/plugin-proposal-export-namespace-from',
+      "react-native-reanimated/plugin",
+    ],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
