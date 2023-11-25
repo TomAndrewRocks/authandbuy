40bf66fbd94d313ee44ce6a6418ae971900e28ff
MarioFerreira <mario@entregali.com.br>
2023-11-25 13:26:43 -0300

    feat/configure eslint prettier and absolute path with aliases

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  root: true,
+  extends: ['universe/native', 'universe/node', 'universe/web'],
+};
--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,43 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "expo-router/babel",
+      'expo-router/babel',
       [
-        "module:react-native-dotenv",
+        'module-resolver',
         {
-          envName: "AUTHBUY_ENV",
-          moduleName: "@env",
-          path: ".env",
+          root: ['.'],
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.ios.tsx',
+            '.android.tsx',
+            '.js',
+            '.ts',
+            '.tsx',
+            '.json',
+          ],
+          alias: {
+            '@components': './app/shared/components',
+            '@themes': './app/shared/themes',
+          },
+        },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'AUTHBUY_ENV',
+          moduleName: '@env',
+          path: '.env',
         },
       ],
       '@babel/plugin-proposal-export-namespace-from',
-      "react-native-reanimated/plugin",
+      'react-native-reanimated/plugin',
     ],
     env: {
       production: {
-        plugins: ["react-native-paper/babel"],
+        plugins: ['react-native-paper/babel'],
       },
     },
   };
